c4692e43fe1a707b1d7fdaff41c8035178cea838
Waleed Almutiry <walee2001d@hotmail.co.uk>
2019-04-21 01:13:26 +0300

    Add files via upload

---
+++ tests/testthat/test_datagen.R
@@ -0,0 +1,138 @@
+library(EpiILMCT)
+
+set.seed(22)
+loc<- matrix(cbind(runif(50, 0, 10),runif(50, 0, 10)), ncol = 2, nrow = 50)
+net1<- contactnet(type = "powerlaw", location = loc, beta = 1.5, 
+	nu = 0.5)
+
+#################################
+#################################
+#################################
+########       SIR       ########
+#################################
+#################################
+#################################
+
+#################################
+########  spatial ILMs:  ########
+#################################
+
+# first: with power-law kernel:
+
+cov1 <- cbind(runif(50, 0, 50), runif(50, 0, 5))
+cov2 <- cbind(runif(50, 0, 50), runif(50, 0, 5))
+
+# with specifying all the options of susceptibility and transmissibility functions:
+
+epi.dist.po1 <- datagen(type = "SIR", kerneltype = "distance", kernelmatrix = loc,
+				initialepi = matrix(c(13, 2, 2, 0), ncol = 4, nrow = 1), tmax = 4,
+				distancekernel = "powerlaw", suspar = c(0.01, 2), transpar = c(0.03,0.2),
+				powersus = c(0.5, 0.7), powertrans = c(0.7, 1.3),
+				kernel.par = 8, delta = c(6,2), suscov = cov1, transcov = cov2,
+				seedval = 299)
+
+epi.dist.po1$epidat
+
+loglikelihoodepiILM(object = epi.dist.po1, distancekernel = "powerlaw", 
+control.sus = list(c(0.01,2), cov1, c(0.5,0.7)), control.trans = list(c(0.03,0.2), cov2, c(0.7,1.3)), 
+kernel.par = 8, delta = c(6,2))
+
+epi.dist.po2 <- datagen(type = "SIR", kerneltype = "network", kernelmatrix = net1$contact.network,
+				initialepi = matrix(c(13, 2, 2, 0), ncol = 4, nrow = 1), tmax = 4,
+				suspar = c(0.01, 2), transpar = c(0.03,0.2),
+				powersus = c(0.5, 0.7), powertrans = c(0.7, 1.3),
+				delta = c(6,2), suscov = cov1, transcov = cov2,
+				seedval = 299)
+
+epi.dist.po2$epidat
+
+loglikelihoodepiILM(object = epi.dist.po2,
+control.sus = list(c(0.01,2), cov1, c(0.5,0.7)), control.trans = list(c(0.03,0.2), cov2, c(0.7,1.3)), 
+delta = c(6,2))
+
+epi.dist.po3 <- datagen(type = "SIR", kerneltype = "both", kernelmatrix = list(loc,net1$contact.network),
+				initialepi = matrix(c(13, 2, 2, 0), ncol = 4, nrow = 1), tmax = 4,
+				distancekernel = "powerlaw", suspar = c(0.01, 2), transpar = c(0.03,0.2),
+				powersus = c(0.5, 0.7), powertrans = c(0.7, 1.3),
+				kernel.par = c(8,0.3), delta = c(6,2), suscov = cov1, transcov = cov2,
+				seedval = 299)
+
+epi.dist.po3$epidat
+
+loglikelihoodepiILM(object = epi.dist.po3, distancekernel = "powerlaw", 
+control.sus = list(c(0.01,2), cov1, c(0.5,0.7)), control.trans = list(c(0.03,0.2), cov2, c(0.7,1.3)), 
+kernel.par = c(8,0.3), delta = c(6,2))
+
+data(NetworkDataSINR)
+names(NetworkDataSINR)
+
+
+netSIR<-as.epidat(type = "SIR", kerneltype = "network", inf.time = NetworkDataSINR$epi[,6], rem.time = NetworkDataSINR$epi[,2], id.individual = NetworkDataSINR$epi[,1], location  = NetworkDataSINR$loc, network = NetworkDataSINR$net, network.type = "powerlaw")
+
+netSIR<-as.epidat(type = "SIR", kerneltype = "distance", inf.time = NetworkDataSINR$epi[,6], rem.time = NetworkDataSINR$epi[,2], id.individual = NetworkDataSINR$epi[,1], location  = NetworkDataSINR$loc, network = NetworkDataSINR$net, network.type = "Cauchy")
+
+#################################
+#################################
+#################################
+########       SINR      ########
+#################################
+#################################
+#################################
+
+#################################
+########  spatial ILMs:  ########
+#################################
+
+# first: with power-law kernel:
+
+cov1 <- cbind(runif(50, 0, 50), runif(50, 0, 5))
+cov2 <- cbind(runif(50, 0, 50), runif(50, 0, 5))
+
+# with specifying all the options of susceptibility and transmissibility functions:
+
+epi.dist.po1 <- datagen(type = "SINR", kerneltype = "distance", kernelmatrix = loc,
+				initialepi = matrix(c(13, 2, 1, 1, 1, 0), ncol = 6, nrow = 1), tmax = 4,
+				distancekernel = "powerlaw", suspar = c(0.01, 2), transpar = c(0.03,0.2),
+				powersus = c(0.5, 0.7), powertrans = c(0.7, 1.3),
+				kernel.par = 8, delta = matrix(c(1,2,6,2), ncol = 2, byrow = TRUE), 
+				suscov = cov1, transcov = cov2,
+				seedval = 299)
+
+epi.dist.po1$epidat
+
+loglikelihoodepiILM(object = epi.dist.po1, distancekernel = "powerlaw", 
+control.sus = list(c(0.01,2), cov1, c(0.5,0.7)), control.trans = list(c(0.03,0.2), cov2, c(0.7,1.3)), 
+kernel.par = 8, delta = matrix(c(1,2,6,2), ncol = 2, byrow = TRUE))
+
+
+epi.dist.po2 <- datagen(type = "SINR", kerneltype = "network", kernelmatrix = net1,
+				initialepi = matrix(c(13, 2, 1, 1, 1, 0), ncol = 6, nrow = 1), tmax = 2,
+				suspar = c(0.01, 2), transpar = c(0.03,0.2),
+				powersus = c(0.5, 0.7), powertrans = c(0.7, 1.3),
+				delta = matrix(c(1,2,6,2), ncol = 2, byrow = TRUE), 
+				suscov = cov1, transcov = cov2,
+				seedval = 299)
+epi.dist.po2$epidat
+
+loglikelihoodepiILM(object = epi.dist.po2, distancekernel = "powerlaw", 
+control.sus = list(c(0.01,2), cov1, c(0.5,0.7)), control.trans = list(c(0.03,0.2), cov2, c(0.7,1.3)), 
+delta = matrix(c(1,2,6,2), ncol = 2, byrow = TRUE))
+
+epi.dist.po3 <- datagen(type = "SINR", kerneltype = "both", kernelmatrix = list(loc,net1$contact.network),
+				initialepi = matrix(c(13, 2, 1, 1, 1, 0), ncol = 6, nrow = 1), tmax = 2,
+				distancekernel = "powerlaw", suspar = c(0.01, 2), transpar = c(0.03,0.2),
+				powersus = c(0.5, 0.7), powertrans = c(0.7, 1.3),
+				kernel.par = c(8,0.3), delta = matrix(c(1,2,6,2), ncol = 2, byrow = TRUE), 
+				suscov = cov1, transcov = cov2,
+				seedval = 299)
+
+epi.dist.po3$epidat
+
+data(NetworkDataSINR)
+names(NetworkDataSINR)
+
+
+netSINR<-as.epidat(type = "SINR", kerneltype = "network", incub.time = NetworkDataSINR$epi[,4], inf.time = NetworkDataSINR$epi[,6], rem.time = NetworkDataSINR$epi[,2], id.individual = NetworkDataSINR$epi[,1], location  = NetworkDataSINR$loc, network = NetworkDataSINR$net, network.type = "powerlaw")
+
+
+netSINR<-as.epidat(type = "SINR", kerneltype = "distance", incub.time = NetworkDataSINR$epi[,4], inf.time = NetworkDataSINR$epi[,6], rem.time = NetworkDataSINR$epi[,2], id.individual = NetworkDataSINR$epi[,1], location  = NetworkDataSINR$loc, network = NetworkDataSINR$net, network.type = "Cauchy")
---
+++ tests/testthat/test_epictmcmc3.R
@@ -0,0 +1,142 @@
+library(EpiILMCT)
+## distance-based SIR continuous-time ILMs ##
+data(SpatialData)
+## performing the MCMC-tool for analyzing the fully observed spatial data 
+## under the SIR distance-based continuous ILM:
+suspar <- list(NULL)
+suspar[[1]]<-list(2,c("gamma", 1, 0.01, 0.5))
+suspar[[2]]<- rep(1,length(SpatialData$epidat[,1]))
+kernel1 <- list(2, c("gamma", 1, 0.01, 0.5))
+
+mcmcres2 <- epictmcmc(object = SpatialData,
+distancekernel = "powerlaw", datatype = "known epidemic", nsim = 50, 
+control.sus = suspar,
+kernel.par = kernel1,  seedval  =  524837)
+plot(mcmcres2)
+print(mcmcres2)
+summary(mcmcres2)
+
+suspar <- list(NULL)
+suspar[[1]]<-list(2,c("gamma", 1, 0.1, 2.5))
+suspar[[2]]<- matrix(rep(1,length(SpatialData$epidat[,1])),ncol=1)
+kernel1 <- list(0.2, c("gamma", 1, 0.1, 0.01))
+
+mcmcres22 <- epictmcmc(object = SpatialData,
+distancekernel = "Cauchy", datatype = "known epidemic", nsim = 50, 
+control.sus = suspar,
+kernel.par = kernel1,  seedval  =  524837)
+plot(mcmcres22)
+print(mcmcres22)
+summary(mcmcres22)
+
+plot(mcmcres2$log.likelihood)
+plot(mcmcres22$log.likelihood)
+
+## performing the MCMC-tool for analyzing the partially observed spatial
+## data (unknown infection times) under the SIR distance-based 
+## continuous ILM:
+
+suspar <- list(NULL)
+suspar[[1]]<-list(2,c("gamma", 1, 0.01, 0.8))
+suspar[[2]]<- matrix(rep(1,length(SpatialData$epidat[,1])),ncol=1)
+kernel1 <- list(2, c("gamma", 1, 0.01, 0.5))
+
+mcmcres22 <- epictmcmc(object = SpatialData, distancekernel = "powerlaw",
+datatype = "known removal", nsim = 50,  
+control.sus = suspar, kernel.par = kernel1, delta = list(1, 2, c(4, 2)),  
+seedval  =  524837)
+
+plot(mcmcres22)
+print(mcmcres22)
+summary(mcmcres22)
+
+## distance-based and network-based SIR ILMs ##
+data(SpatialNetData)
+## performing the MCMC-tool for analyzing the fully observed spatial and 
+## network data
+## under the SIR distance-based and network-based continuous-time ILM:
+suspar <- list(NULL)
+suspar[[1]]<-list(c(0.08,0.2),matrix(c("gamma", "gamma", 1, 1, 0.01, 0.01, 0.1, 0.5), 
+ncol = 4, nrow = 2))
+suspar[[2]]<- SpatialNetData[[2]]
+kernel1 <- list(c(5, 0.5), matrix(c("gamma", "gamma", 1, 1,
+0.01, 0.01, 0.5, 1), ncol = 4, nrow = 2))
+
+mcmcres3 <- epictmcmc(object = SpatialNetData[[1]], distancekernel = "powerlaw",
+datatype = "known epidemic", nsim = 50, 
+control.sus = suspar, kernel.par = kernel1, 
+seedval  =  524837)
+plot(mcmcres3)
+print(mcmcres3)
+summary(mcmcres3)
+
+## network-based SIR ILMs ##
+data(NetworkData)
+## performing the MCMC-tool for analyzing the fully observed network data 
+## under the SIR network-based continuous-time ILM:
+
+suspar <- list(NULL)
+suspar[[1]]<-list(c(0.08,0.5),matrix(c("gamma", "gamma", 1, 1, 1, 1, 0.1, 0.5), 
+ncol = 4, nrow = 2))
+suspar[[2]]<- NetworkData[[2]]
+
+mcmcres4 <- epictmcmc(object = NetworkData[[1]], datatype = "known epidemic",
+nsim = 50, control.sus = suspar,  seedval  =  524837)
+plot(mcmcres4)
+print(mcmcres4)
+summary(mcmcres4)
+
+## network-based SINR ILMs ##
+data(NetworkDataSINR)
+names(NetworkDataSINR)
+
+
+netSINR<-as.epidat(type = "SINR", kerneltype = "network", incub.time = NetworkDataSINR$epi[,4], inf.time = NetworkDataSINR$epi[,6], rem.time = NetworkDataSINR$epi[,2], id.individual = NetworkDataSINR$epi[,1], location  = NetworkDataSINR$loc, network = NetworkDataSINR$net, network.type = "powerlaw")
+
+## performing the MCMC-tool for analyzing the fully observed network data
+## under the SINR network-based continuous-time ILM:
+suspar <- list(NULL)
+suspar[[1]]<-list(c(0.08,0.2),matrix(c("gamma", "gamma", 1, 1, 0.01, 0.01, 0.05, 0.5), 
+ncol = 4, nrow = 2))
+suspar[[2]]<- NetworkDataSINR$cov
+
+mcmcres5 <- epictmcmc(object = netSINR, datatype = "known epidemic",
+nsim = 500, control.sus = suspar, seedval  =  524837)
+
+mcmcres5
+plot(mcmcres5)
+print(mcmcres5)
+summary(mcmcres5)
+
+suspar <- list(NULL)
+suspar[[1]]<-list(c(0.08,0.2),matrix(c("gamma", "gamma", 1, 1, 0.01, 0.01, 0.05, 0.5), 
+ncol = 4, nrow = 2))
+suspar[[2]]<- NetworkDataSINR$cov
+delta1<-list(1,2,c(4,2))
+spark<-list(1,matrix(c("gamma", 1, 0.01, 0.05), ncol = 4, nrow = 1))
+
+mcmcres5 <- epictmcmc(object = netSINR, datatype = "known removal",
+nsim = 500, control.sus = suspar, spark.par = spark, delta = delta1, seedval  =  524837)
+
+print(mcmcres5)
+summary(mcmcres5)
+plot(mcmcres5)
+
+suspar <- list(NULL)
+suspar[[1]]<-list(c(0.08,0.2),matrix(c("gamma", "gamma", 1, 1, 0.01, 0.01, 0.05, 0.5), 
+ncol = 4, nrow = 2))
+suspar[[2]]<- NetworkDataSINR$cov
+delta1<-list(NULL)
+delta1[[1]]<-c(1,1)
+delta1[[2]]<-c(2,2)
+delta1[[3]]<-matrix(c(4,4,2,2),ncol=2,nrow=2)
+spark<-list(1,matrix(c("gamma", 1, 0.01, 0.05), ncol = 4, nrow = 1))
+
+mcmcres5 <- epictmcmc(object = netSINR, datatype = "unknown removal", 
+nsim = 500, control.sus = suspar, spark.par = spark, delta = delta1,seedval  =  524837)
+
+print(mcmcres5)
+summary(mcmcres5)
+plot(mcmcres5, ask = FALSE)
+
+
